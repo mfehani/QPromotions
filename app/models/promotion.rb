cc8275c0b8690c5c99c6c8ab60da292ef930150f
Maryam Ahmed <maryamf@qatar.cmu.edu>
2014-02-24 14:50:36 +0300

    yazan commenting

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,8 +1,7 @@
 class Promotion < ActiveRecord::Base
   attr_accessible :name, :discount, :branch, :category, :location, :description, :valid
   
-  validates :name, :discount, :branch, :category, :location, :description 
-  #, #:presence => true
+#  validates :name, :discount, :branch, :category, :location, :description , :presence => true
   validates :description, :length => {:maximum => 350}
   validates :name, :length => {:maximum => 50}
   #unique
