69273160e033b5e3899d10d875a956c5979a4e26
Maryam Ahmed <maryamf@qatar.cmu.edu>
2014-02-28 16:51:53 +0300

    ha

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module QPromotions
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     
+    config.assets.initialize_on_precompile = false
     
     config.middleware.use Rack::Cors do
           allow do
