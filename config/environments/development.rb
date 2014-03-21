8a8695faf1025e188f2c24e2bc075661487c130c
Maryam Ahmed <maryamf@qatar.cmu.edu>
2014-03-21 13:51:23 +0300

    i think

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ QPromotions::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'still-shore-7988.herokuapp.com' }
 
   config.assets.initialize_on_precompile = false
 
