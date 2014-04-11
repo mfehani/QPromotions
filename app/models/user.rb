cc372816514e850f44ab5feb1f32a9a06115a9bd
Maryam Ahmed <maryam.a.alfehani@gmail.com>
2014-04-11 22:17:19 +0300

    ugh

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   belongs_to :community
   accepts_nested_attributes_for :branches
   
-  validates :name, :email, :presence => true
-  validates :nationality, :length =>{:minimum => 3}
-  validates :name, :length => {:maximum => 50}
+#  validates :name, :email, :presence => true
+ # validates :nationality, :length =>{:minimum => 3}
+#  validates :name, :length => {:maximum => 50}
 end
